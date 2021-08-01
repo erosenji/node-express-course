1089cd65dec78f3665a25d4d85f53c2abd3511a7
Emerson Moore <erosenjiy@gmail.com>
2021-08-01 17:38:54 -0400

    there was a typo in the fuction login

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 
+//bosy parser is deprected need to see what change was made for this 
+app.use(bodyParser.json());
+
+// data set of names 
 const mockUserData=[
     {name:'ero'},
-    {name:'cindy'}
+    {name:'cindy'},
+    {name: 'bob'},
+    {name: 'tom'}
 ]
 
+
+// login post method 
+app.post('/login',function(req,res){
+const username = req.body.username;
+const password = req.body.password;
+
+const mockUsername = "testuser";
+const mockPassword = "password";
+
+//verify user login credentails  
+if(username === mockUsername && password===mockPassword){
+res.json({
+    success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+})
+}else{
+    res.json({
+        success: false,
+        message: 'password and username do not match'
+   })  
+}
+})
+
+// output user and message 
 app.get('/users',function(req,res){
     res.json({
         success:    true,
@@ -14,6 +47,7 @@ app.get('/users',function(req,res){
     })
 })
 
+//check to see if a user name is in the list 
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -22,7 +56,7 @@ app.get('/users/:id',function(req,res){
 		user: req.params.id
 	})
 })
-
+// setup server port
 app.listen(8000,function(){
     console.log("server is running")
 })
