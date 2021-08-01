a925936fd4341fc93c0d8cb5c2ed1bb4b9ec277c
Emerson Moore <erosenjiy@gmail.com>
2021-08-01 15:03:17 -0400

    added first get route

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+    {name:'ero'},
+    {name:'cindy'}
+]
 
+app.get('/users',function(req,res){
+    res.json({
+        success:    true,
+        message: 'sucessfully got users. Well Done',
+        users:  mockUserData
+    })
+})
 
 app.listen(8000,function(){
     console.log("server is running")
